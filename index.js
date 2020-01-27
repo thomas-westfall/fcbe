7961cf6d6f9c60e76adb2611bf3b44b4feec8309
thomas-westfall <thomas.westfall64@myhunter.cuny.edu>
2020-01-27 01:05:54 -0500

    cors again

--- index.js
+++ index.js
@@ -59,22 +59,22 @@ const syncDatabase = () => {
 // A helper function to create our app with configurations and middleware;
 const configureApp = () => {
 
-  var allowedOrigins = ['http://localhost:3000',
-                      'https://fcfeb.herokuapp.com/'];
-app.use(cors({
-  credentials: true,
-  origin: function(origin, callback){
-    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+//   var allowedOrigins = ['http://localhost:3000',
+//                       'https://fcfeb.herokuapp.com/'];
+// app.use(cors({
+//   credentials: true,
+//   origin: function(origin, callback){
+//     // allow requests with no origin 
+//     // (like mobile apps or curl requests)
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1){
+//       var msg = 'The CORS policy for this site does not ' +
+//                 'allow access from the specified Origin.';
+//       return callback(new Error(msg), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
   app.use(helmet());
   app.use(logger('dev'));
